b9c427c3859a60a081ebc6a926c647d0a18a2884
Jordan Watkins <jmwatkins95@gmail.com>
2026-03-04 09:39:45 -0500

    modified server.js

--- src/server/server.js
+++ src/server/server.js
@@ -115,7 +115,9 @@ Context: ${context || "unknown"}
     })
   }
 })
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../dist/index.html"))
+})
 // ------------------------------
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`)
